be36663c2783db89fe697ee69491569f82d6ca84
Ciaran McCann <ciaran@flax.ie>
2010-09-03 15:04:34 +0000

    name change due to confliction, delete Event.java

---
+++ Flax Engine/src/ie/flax/flaxengine/client/events/EventBus.java
@@ -0,0 +1,15 @@
+package ie.flax.flaxengine.client.events;
+
+import com.google.gwt.event.shared.HandlerManager;
+
+/**
+ * This class has only one property and simply acts as a goble way of acessing the eventBus
+ * 
+ * @author Ciarán McCann
+ *
+ */
+public class EventBus {
+	
+	public static HandlerManager hanlderManger;
+
+}
