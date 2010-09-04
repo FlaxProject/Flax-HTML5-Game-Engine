60cf4183b3664723db74320f30c91a6e53f5d9be
Ciaran McCann <ciaran@flax.ie>
2010-09-04 16:43:20 +0000

    Added Json deserailier to Settings so now it can be instaniziated by passing a path to the settings file. Added a test file for settings.json. Also updated the map.json file.

--- Flax Engine/src/ie/flax/flaxengine/client/Settings.java
+++ Flax Engine/src/ie/flax/flaxengine/client/Settings.java
@@ -1,5 +1,6 @@
 package ie.flax.flaxengine.client;
 
+import ie.flax.flaxengine.client.events.EventBus;
 import ie.flax.flaxengine.client.events.onFileLoadedEvent;
 import ie.flax.flaxengine.client.events.onFileLoadedEventHandler;
 
@@ -17,9 +18,8 @@ import com.allen_sauer.gwt.log.client.Log;
  * @author carllange
  *
  */
-public class Settings implements JsonSerializable{
+public class Settings implements JsonSerializable, onFileLoadedEventHandler{
 	
-
 	private String IMAGE_DIRECTORY_PATH;	//TODO should be lowercase (or final)
 	private String MAP_DIRECTORY_PATH;		//TODO should be lowercase (or final)
 	private Boolean collisionOn;
@@ -35,20 +35,17 @@ public class Settings implements JsonSerializable{
 		collisionOn = true;
 	}
 	
-	
-	
+		
 	/**
 	 * This initialises the settings from an XML file on the server.
 	 * 
 	 * @param settingsXmlFile The name and extension of the XML file on the server.
 	 */
-	public Settings(String Json) {
-		Serializer serializer = (Serializer) GWT.create(Serializer.class);
-		Settings temp = (Settings) serializer.deSerialize(Json,"ie.flax.flaxengine.client.Settings");
+	public Settings(String pathToSettingFile) {
 		
-		this.collisionOn = temp.collisionOn;
-		this.IMAGE_DIRECTORY_PATH = temp.IMAGE_DIRECTORY_PATH;
-		this.MAP_DIRECTORY_PATH = temp.MAP_DIRECTORY_PATH;
+		EventBus.handlerManager.addHandler(onFileLoadedEvent.TYPE, this);	
+		FileHandle.readFileAsString(pathToSettingFile,this.toString());
+		
 	}
 	
 	/**
@@ -74,6 +71,7 @@ public class Settings implements JsonSerializable{
 		IMAGE_DIRECTORY_PATH = imgDirPath;
 		MAP_DIRECTORY_PATH = mapDirPath;
 		collisionOn = collision;
+		
 	}
 
 	
@@ -115,5 +113,46 @@ public class Settings implements JsonSerializable{
 
 	public Boolean getCollision() {
 		return collisionOn;
+	}
+	
+	/**
+	 * Pass this method JSON and it gives you back an FMap object which you can
+	 * then assign to your object via FMap myMap = JsonToFMap(String Json);
+	 * 
+	 * @param JSON
+	 * @return
+	 */
+	public  Settings JsonToSettings(String Json) {
+		Serializer serializer = (Serializer) GWT.create(Serializer.class);
+		return (Settings) serializer.deSerialize(Json,"ie.flax.flaxengine.client.Settings");
+	}
+
+	/**
+	 * Creates a JSON string from the current FMap object
+	 * 
+	 * @return String of JSON
+	 */
+	public String settingsToJson() {
+		Serializer serializer = (Serializer) GWT.create(Serializer.class);
+		return serializer.serialize(this);
+	}
+	
+
+
+
+	@Override
+	public void onFileLoaded(onFileLoadedEvent e) {
+	
+		if(this.toString().equalsIgnoreCase(e.getId()))
+		{
+		Settings temp = JsonToSettings(e.getDataLoadedFromFile());
+		
+		this.collisionOn = temp.collisionOn;
+		this.IMAGE_DIRECTORY_PATH = temp.IMAGE_DIRECTORY_PATH;
+		this.MAP_DIRECTORY_PATH = temp.MAP_DIRECTORY_PATH;
+		
+		Log.info("The settings object was constructed from a file sucessfully");
+		}
+	
 	}
 }
