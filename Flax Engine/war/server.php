60f3d6c1deeb2a621f1bd501511f9b52e233d2f9
Ciaran McCann <ciaran@flax.ie>
2010-11-08 20:59:32 +0000

    Working server PHP script, been test with.

--- Flax Engine/war/server.php
+++ Flax Engine/war/server.php
@@ -7,25 +7,31 @@
  * 
  * @author Ciarán McCann
  */ 
+ 
+ //This file most have write permissions on the server to work
 
 /*********BELOW IS THE AUTH_KEY WHICH MOST BE MODIFED TO MAKE THE CLIENT CODE***********/
-	constant $AUTHORIZATION_KEY = 0;
+	define("AUTHORIZATION_KEY", 0);
 /*********ABOVE IS THE AUTH_KEY WHICH MOST BE MODIFED TO MAKE THE CLIENT CODE***********/
 
-	//	 DOTO AS EXTRA SEACUREITY ADD ONLY WRITES TO JSON FILES IN WAR DIR
-	if($AUTHORIZATION_KEY == htmlentities($_POST['uid']))
+	//DOTO AS EXTRA SEACUREITY ADD ONLY WRITES TO JSON FILES IN WAR DIR
+	if(AUTHORIZATION_KEY == htmlentities($_POST['uid']))
 	{
+	
 		//Secure $_POST input
 		$fileName = htmlentities($_POST['fileName']);				
 		$fileContents = htmlentities($_POST['fileContents']);
 
 
-		$file = fopen($fileName, 'w') or die("can't open file");	//Opens file
-		fwrite($file, $fileContents);								//Writes to file
-		fclose($file);												//Close file
+		$file = fopen($fileName, 'w') or die("File cant be opened");		//Opens file
+		fwrite($file, $fileContents) or die("File cannot be written");				//Writes to file
+		fclose($file);							//Close file
+		
 	}else{
 		
 		die("AUTHORIZATION_KEY did not match");
 	}
 		
 		
+		
+?>
