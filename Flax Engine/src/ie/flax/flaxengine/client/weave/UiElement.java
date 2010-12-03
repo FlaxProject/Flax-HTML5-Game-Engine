1d09fd5d8eac06fee4abb2aac2c0ab6aec8cf347
Ciaran McCann <ciaran@flax.ie>
2010-12-03 18:40:49 +0000

    Added UIelements which gives a list of UI elements in weave which the developer can update and allows the HTML to be modifted without braking code. Also weave can now be hide() and show(). Also UIelements can now be appended.

---
+++ Flax Engine/src/ie/flax/flaxengine/client/weave/UiElement.java
@@ -0,0 +1,25 @@
+package ie.flax.flaxengine.client.weave;
+
+/**
+ * Lists the ID's of elements in the UI of weave
+ * @author ciaran
+ *
+ */
+public class UiElement {
+	
+	/**
+	 * Where the numbers for the FPS counter are displayed, in the bottom weave panel
+	 */
+	static public final String FPS_COUNTER_BOTTOM_PANEL = "fpscount"; 
+	
+	/**
+	 * Where the output for the logger is piped, in the bottom weave panel
+	 */
+	static public final String LOGGER_BOTTOM_PANEL = "log"; 
+	
+	/**
+	 * Complete Weave UI
+	 */
+	static public final String WEAVE_UI = "weavebottomPanel";
+
+}
--- Flax Engine/src/ie/flax/flaxengine/client/weave/Weave.java
+++ Flax Engine/src/ie/flax/flaxengine/client/weave/Weave.java
@@ -18,29 +18,39 @@ public class Weave {
 	 */
 	public Weave(String insertID, int width, int height)
 	{
-		bottomPanel = new HTMLPanel("<div id=\"weavebottomPanel\" style=\"width:"+ width +"px;height:" + 90 + "px;\">" +
+		bottomPanel = new HTMLPanel("<div id=\"weavebottomPanel\" class=\"weaveHide\" style=\"width:"+ width +"px;height:" + 90 + "px;\">" +
 				"<div id=\"fps\">" +
 				"<p class=\"header\">FPS</p><h2 id=\"fpscount\"></h2></div>" +
 				"<div id=\"log\"><p class=\"header\">Logger</p></div>  " +
 				"</div>");
 		
 		this.insertId = insertID;	
+		RootPanel.get(insertId).add(bottomPanel);
 	}
 	
 	/**
 	 * Displays the UI
 	 */
-	public void display()
+	public void show()
 	{
-		RootPanel.get(insertId).add(bottomPanel);
+		RootPanel.get(UiElement.WEAVE_UI).setStylePrimaryName("weaveShow");
+	}
+	
+	/**
+	 * Displays the UI
+	 */
+	public void hide()
+	{
+		RootPanel.get(UiElement.WEAVE_UI).setStylePrimaryName("weaveHide");
 	}
 	
 	/**
 	 * Given an ID and content it inserts the content into element with provided ID
+	 * To get a list of UI elements to update use UiElement.yourElementName. You should NOT raw string ID's. 
 	 * @param id
 	 * @param content
 	 */
-   public void updateUIelement(String id,String content)
+   public void updateUIelement(final String id,String content)
 	{
 	   //checks first if the element with ID=id exists
 		if (RootPanel.get(id) != null) {
@@ -52,5 +62,24 @@ public class Weave {
 		}
 	}
 	
+   
+   /**
+	 * Given an ID and content it appends the content into element with provided ID
+	 * To get a list of UI elements to update use UiElement.yourElementName. You should NOT raw string ID's. 
+	 * @param id
+	 * @param content
+	 */
+  public void appendUIelement(final String id,String content)
+	{
+	   //checks first if the element with ID=id exists
+		if (RootPanel.get(id) != null) {
+			RootPanel.get(id).add(new HTMLPanel(content));//Inserts the content
+		} else {
+
+			Log.warn("Update of UI element [" + id + "] failed due to it been null.");
+		}
+	}
+   
+   
 
 }
