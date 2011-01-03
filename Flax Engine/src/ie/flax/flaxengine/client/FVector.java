69d68556f57b940cb5e079c4add8aa7902e24261
Ciaran McCann <ciaran@flax.ie>
2011-01-03 21:01:25 +0000

    Fixed bugs in FVector class and also added in commenting.

--- Flax Engine/src/ie/flax/flaxengine/client/FVector.java
+++ Flax Engine/src/ie/flax/flaxengine/client/FVector.java
@@ -1,22 +1,74 @@
 package ie.flax.flaxengine.client;
 
+/**
+ * 2D Vector Class
+ *
+ */
 public class FVector {
+	
 	public double x,y;
 	
-	FVector(double x, double y){
+	/**
+	 * Takes an x double and y double
+	 * @param x
+	 * @param y
+	 */
+	public FVector(double x, double y){
 		this.x = x;
 		this.y = y;
 	}
 
-	FVector add(FVector v){
-		return new FVector(this.x+=v.x,this.y+=v.y);
+	/**
+	 * Returns new vector with v and this added togtheir
+	 * @param v
+	 * @return
+	 */
+	public FVector add(FVector v){
+		return new FVector(this.x+v.x,this.y+v.y);
 	}
 	
-	FVector subtract(FVector v){
-		return new FVector(this.x-=v.x,this.y-=v.y);
+	/**
+	 * Returns new vector with v and this added togtheier
+	 * @param v
+	 * @return
+	 */
+	public FVector subtract(FVector v){
+		return new FVector(this.x-v.x,this.y-v.y);
 	}
 	
-	void rotate(double angleInDegrees){
+	/**
+	 * Scales the vectors componemts by F
+	 * @param f
+	 * @return
+	 */
+	public FVector scale(float f){
+		return new FVector(this.x*f, this.y*f);
+	}
+	
+	/**
+	 * 
+	 * @param v
+	 * @return
+	 */
+	public double dotProduct(FVector v){
+		return ((x*(v.x))+(y*(v.y)));
+	}
+	
+	/**
+	 * Gets the lenght of the current vector
+	 * @return
+	 */
+	public double lenght(){
+		return Math.sqrt((x*x) + (y*y));
+	}
+	
+	
+	/**
+	 * Retruns a new vector which has been roated by given amount
+	 * @param angleInDegrees
+	 * @return
+	 */
+	public FVector rotate(double angleInDegrees){
 		angleInDegrees=angleInDegrees/360.0*2*Math.PI;
 
 		double a,b;
@@ -24,20 +76,9 @@ public class FVector {
 		a= x*Math.cos(angleInDegrees)+y*Math.sin(angleInDegrees);
 		b= x*-Math.sin(angleInDegrees)+y*Math.cos(angleInDegrees);
 
-		x=a;
-		y=b;
+		return new FVector(a,b);
 	}
+
+
 	
-	void scale(float f){
-		x*=f;
-		y*=f;
-	}
-	
-	double dot(FVector v){
-		return ((x*(v.x))+(y*(v.y)));
-	}
-	
-	double magnitude(){
-		return Math.sqrt((x*x) + (y*y));
-	}
 }
