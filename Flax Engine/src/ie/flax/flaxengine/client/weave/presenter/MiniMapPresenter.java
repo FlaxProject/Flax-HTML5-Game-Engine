241d2688baad023b392877d0a8ac0194b8d08076
Carl Lange <carl@flax.ie>
2011-07-02 14:12:23 +0100

    Minimap works better now - displays all, refreshes less (once a second), moves the main camera to the mouse-click.
    Change-Id: I3bddcdad3693584825e7c3ac54f03f436dcee9eb

--- Flax Engine/src/ie/flax/flaxengine/client/weave/presenter/MiniMapPresenter.java
+++ Flax Engine/src/ie/flax/flaxengine/client/weave/presenter/MiniMapPresenter.java
@@ -3,51 +3,57 @@ package ie.flax.flaxengine.client.weave.presenter;
 import ie.flax.flaxengine.client.FVector;
 import ie.flax.flaxengine.client.FlaxEngine;
 import ie.flax.flaxengine.client.Graphic.FCamera;
-import ie.flax.flaxengine.client.Graphic.Graphic;
-import ie.flax.flaxengine.client.Graphic.TimerCallback;
 import ie.flax.flaxengine.client.weave.Weave;
 import ie.flax.flaxengine.client.weave.view.MiniMapView;
 import ie.flax.flaxengine.client.weave.view.Impl.MiniMapViewImpl;
 
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Widget;
 
 public class MiniMapPresenter extends AbstractPresenter implements
-		MiniMapView.presenter {
+        MiniMapView.presenter {
 
-	private final Weave model;
-	private final FCamera cam;
-	private final MiniMapView v;
-	
-	//FIXME CARL - Before starting work remeber we need to remove the hidden files gwt-log 
-	// from the tracking and also make your classpath match mine. I have removed all other files that
-	// I can though the EGIT interface
+    private final Weave model;
+    private final FCamera cam;
+    private final MiniMapView v;
+    private final int inverseScale = 8; // eg 8 if you want 1/8 scale
+    Timer timer = new Timer() {
+        @Override
+        public void run() {
+            model.getFMapReference().draw(cam, v.getCanvas());
+        }
+    };
 
-	TimerCallback drawLoop = new TimerCallback() {
+    public MiniMapPresenter(Weave model) {
+        this.model = model;
+        v = new MiniMapViewImpl(this);
+        cam = new FCamera(new FVector(0, 0), FlaxEngine.camera.getWidth()
+                * inverseScale, FlaxEngine.camera.getHeight());
+        v.getCanvas().getContext2d()
+                .scale(1.0 / inverseScale, 1.0 / inverseScale);
 
-		@Override
-		public void fire() {
-			Graphic.getSingleton().requestAnimationFrame(drawLoop);
-			model.getFMapReference().draw(cam, v.getCanvas());
-		}
-	};
+        /*
+         * Once, I used requestAnimationFrame here. However, there are two
+         * things wrong with this. One is that this _doesn't need to refresh
+         * that often_. Users won't notice. The other is that it makes much more
+         * sense to just update the minimap when and only when the main map is
+         * changed. Compromise for the moment - timer that refreshes every
+         * second.
+         */
 
-	public MiniMapPresenter(Weave model) {
-		this.model = model;
-		cam = new FCamera(new FVector(0, 0), 500, 500);
-		v = new MiniMapViewImpl(this);
-		// set up draw loop here
-		Graphic.getSingleton().requestAnimationFrame(drawLoop);
-	}
+        // TODO Carl change to event-based?
+        timer.scheduleRepeating(1000);
+    }
 
-	@Override
-	public Widget getView() {
-		// TODO Auto-generated method stub
-		return v.getWidget();
-	}
+    @Override
+    public Widget getView() {
+        return v.getWidget();
+    }
 
-	@Override
-	public void moveMapCamera(int x, int y) {
-		FlaxEngine.camera.setX(x);
-		FlaxEngine.camera.setY(y);
-	}
+    @Override
+    public void moveMapCamera(int x, int y) {
+        // divided by two to offset the move. TODO Carl make this better
+        FlaxEngine.camera.setX((x / 2) * inverseScale);
+        FlaxEngine.camera.setY((y / 2) * inverseScale);
+    }
 }
--- Flax Engine/src/ie/flax/flaxengine/client/weave/view/Impl/MiniMapViewImpl.java
+++ Flax Engine/src/ie/flax/flaxengine/client/weave/view/Impl/MiniMapViewImpl.java
@@ -8,35 +8,36 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Widget;
 
 public class MiniMapViewImpl implements MiniMapView {
-	private final Canvas minimap;
-	private final MiniMapView.presenter presenter;
+    private final Canvas minimap;
+    private final MiniMapView.presenter presenter;
 
-	public MiniMapViewImpl(MiniMapView.presenter pres) {
-		presenter = pres;
-		// create the canvas here
-		minimap = Canvas.createIfSupported();
-		minimap.setSize("100%", "100%");
-		minimap.setCoordinateSpaceHeight(1000);
-		minimap.setCoordinateSpaceWidth(1000);
+    public MiniMapViewImpl(MiniMapView.presenter pres) {
+        presenter = pres;
+        // create the canvas here
+        minimap = Canvas.createIfSupported();
+        minimap.setSize("100%", "100%");
+        minimap.setCoordinateSpaceHeight(1500);
+        minimap.setCoordinateSpaceWidth(1500);
 
-		minimap.addClickHandler(new ClickHandler() {
+        minimap.addClickHandler(new ClickHandler() {
 
-			@Override
-			public void onClick(ClickEvent event) {
-				presenter.moveMapCamera(event.getClientX(), event.getClientY());
-			}
-		});
-	}
+            @Override
+            public void onClick(ClickEvent event) {
+                presenter.moveMapCamera(
+                        event.getRelativeX(minimap.getElement()),
+                        event.getRelativeY(minimap.getElement()));
+            }
+        });
+    }
 
-	@Override
-	public Canvas getCanvas() {
-		// TODO Auto-generated method stub
-		return minimap;
-	}
+    @Override
+    public Canvas getCanvas() {
+        return minimap;
+    }
 
-	@Override
-	public Widget getWidget() {
-		return minimap;
-	}
+    @Override
+    public Widget getWidget() {
+        return minimap;
+    }
 
 }
