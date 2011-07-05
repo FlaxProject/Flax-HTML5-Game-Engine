ff48fd91184eabbe18f8a9fa78043649fb4efbe4
CiaranMcCann <ciaran@flax.ie>
2011-07-05 21:26:50 +0100

    Addded target blank to the links

--- Flax Engine/src/ie/flax/flaxengine/client/weave/presenter/WeavePresenter.java
+++ Flax Engine/src/ie/flax/flaxengine/client/weave/presenter/WeavePresenter.java
@@ -35,25 +35,30 @@ public class WeavePresenter extends AbstractPresenter {
 
 		// TODO Carl Create some kind of strings/help file class for this string and others like it.
 		// TODO Shoudn't really be using plain html in strings.
-		String logo = "<a href='http://flax.ie'><img style='padding-bottom:5px' src='http://flax.ie/images/flaxLogo.png' width='100%'/></a>";
+		String logo = "<a href='http://flax.ie' target='_blank'><img style='padding-bottom:5px' src='http://flax.ie/images/flaxLogo.png' width='100%'/></a>";
 		String quickInstructions;
 		String tiling = "<strong>Tiling: </strong> To tile, select a tile from the tilesheet below " +
 				"and click on the map on the left. You can use any image as a tilesheet, just press " +
 				"<em>Select Tilesheet</em> at the bottom left.";
+		
 		String move = "<strong>Tiling on move:</strong> To tile where your mouse is moving to, hold down shift and " +
 				"simply move your mouse. Don't forget to select a tile!";
+		
 		String regions = "<strong>Tiling regions:</strong> To tile a region, hold down shift and click on the map. "
 				+ "Then drag out from that point. You'll see a red box form. "
 				+ "When you let go, the tiles in that box will be textured with the current texture.";
+		
 		String twitterPlug = "<br/><p style='text-align:center'>You should follow us on Twitter " +
-				"<a href='http://twitter.com/flaxproject'>here</a>.";
-		String sitePlug = "Read more about this project at <a href='http://flax.ie'>flax.ie</a>!</p>";
+				"<a href='http://twitter.com/flaxproject' target='_blank'>here</a>.";
+		
+		String sitePlug = "Read more about this project at <a href='http://flax.ie' target='_blank'>flax.ie</a>!</p>";
 		
 		quickInstructions = logo + "<br/>" + tiling + "<br/>" + move + "<br/>" + regions + "<br/>" + twitterPlug + "<br/>" + sitePlug;
+		
 		display.addToEast(new HTML(quickInstructions), "Help Menu");
 		display.addToEast(new Label("Next iteration, sorry!"), "Create Entity Type");
 		//display.addToEast(new Label("Next iteration, sorry!"), "Entity Type List");
-		display.addToEast(new Label("Next iteration, sorry!"), "Entity List");
+		//display.addToEast(new Label("Next iteration, sorry!"), "Entity List");
 
 		display.addToNorth(new MainMenuView(editor).asWidget()); // None MVP include of UI
 	}
