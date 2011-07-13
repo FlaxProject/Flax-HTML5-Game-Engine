e266668ae51e7d0465a7a769e61ae67af5c2b882
CiaranMcCann <ciaran@flax.ie>
2011-07-13 11:17:53 +0100

    finished wiring for the MiniMap update event.

--- Flax Engine/src/ie/flax/flaxengine/client/weave/presenter/MiniMapPresenter.java
+++ Flax Engine/src/ie/flax/flaxengine/client/weave/presenter/MiniMapPresenter.java
@@ -3,8 +3,10 @@ package ie.flax.flaxengine.client.weave.presenter;
 import ie.flax.flaxengine.client.FVector;
 import ie.flax.flaxengine.client.FlaxEngine;
 import ie.flax.flaxengine.client.Graphic.FCamera;
+import ie.flax.flaxengine.client.events.EventBus;
 import ie.flax.flaxengine.client.events.MiniMapUpdateEvent;
 import ie.flax.flaxengine.client.events.MiniMapUpdateEventHandler;
+import ie.flax.flaxengine.client.events.onImageLoadedEvent;
 import ie.flax.flaxengine.client.weave.Weave;
 import ie.flax.flaxengine.client.weave.view.MiniMapView;
 import ie.flax.flaxengine.client.weave.view.Impl.MiniMapViewImpl;
@@ -16,24 +18,24 @@ public class MiniMapPresenter extends AbstractPresenter implements MiniMapView.p
 
 	private final Weave model;
 	private final FCamera cam;
-	private final MiniMapView v;
+	private final MiniMapView view;
 	private final int inverseScale = 12; // eg 8 if you want 1/8 scale
 	Timer timer = new Timer() {
 		@Override
 		public void run() {
 			clear();
 			
-			if (v.getCanvas().getCoordinateSpaceHeight() == 0) {
-				v.getCanvas().setCoordinateSpaceHeight(
-						v.getCanvas().getCanvasElement().getClientHeight());
-				v.getCanvas().setCoordinateSpaceWidth(
-						v.getCanvas().getCanvasElement().getClientWidth());
-				v.getCanvas().getContext2d()
+			if (view.getCanvas().getCoordinateSpaceHeight() == 0) {
+				view.getCanvas().setCoordinateSpaceHeight(
+						view.getCanvas().getCanvasElement().getClientHeight());
+				view.getCanvas().setCoordinateSpaceWidth(
+						view.getCanvas().getCanvasElement().getClientWidth());
+				view.getCanvas().getContext2d()
 						.scale(1.0 / inverseScale, 1.0 / inverseScale);
 			}
 			
 			
-			model.getFMapReference().draw(cam, v.getCanvas());
+			model.getFMapReference().draw(cam, view.getCanvas());
 			drawCurrentCameraRectangle();
 		}
 	};
@@ -56,12 +58,14 @@ public class MiniMapPresenter extends AbstractPresenter implements MiniMapView.p
 	
 
 	public MiniMapPresenter(Weave model) {
+		
 		this.model = model;
-		v = new MiniMapViewImpl(this);
-		cam = new FCamera(new FVector(0, 0), FlaxEngine.camera.getWidth()
-				* inverseScale, FlaxEngine.camera.getHeight() *inverseScale);
-		v.getCanvas().getContext2d()
-				.scale(1.0 / inverseScale, 1.0 / inverseScale);
+		view = new MiniMapViewImpl(this);
+		cam = new FCamera(new FVector(0, 0), FlaxEngine.camera.getWidth()* inverseScale, FlaxEngine.camera.getHeight() *inverseScale);
+		
+		view.getCanvas().getContext2d().scale(1.0 / inverseScale, 1.0 / inverseScale);
+		
+		EventBus.handlerManager.addHandler(MiniMapUpdateEvent.TYPE, this);
 
 		/*
 		 * Once, I used requestAnimationFrame here. However, there are two
@@ -78,7 +82,7 @@ public class MiniMapPresenter extends AbstractPresenter implements MiniMapView.p
 
 	@Override
 	public Widget getView() {
-		return v.getWidget();
+		return view.getWidget();
 	}
 
 	@Override
@@ -90,9 +94,9 @@ public class MiniMapPresenter extends AbstractPresenter implements MiniMapView.p
 
 	@Override
 	public void drawCurrentCameraRectangle() {
-		v.getCanvas().getContext2d().setStrokeStyle("#F00");
-		v.getCanvas().getContext2d().setLineWidth(8.0);
-		v.getCanvas().getContext2d().strokeRect(
+		view.getCanvas().getContext2d().setStrokeStyle("#F00");
+		view.getCanvas().getContext2d().setLineWidth(8.0);
+		view.getCanvas().getContext2d().strokeRect(
 				FlaxEngine.camera.getX(), 
 				FlaxEngine.camera.getY(), 
 				FlaxEngine.camera.getWidth(), 
@@ -100,9 +104,9 @@ public class MiniMapPresenter extends AbstractPresenter implements MiniMapView.p
 	}
 	
 	private void clear() {
-		v.getCanvas().getContext2d().fillRect(0, 0,
-				v.getCanvas().getCoordinateSpaceWidth()*inverseScale,
-				v.getCanvas().getCoordinateSpaceHeight()*inverseScale);
+		view.getCanvas().getContext2d().fillRect(0, 0,
+				view.getCanvas().getCoordinateSpaceWidth()*inverseScale,
+				view.getCanvas().getCoordinateSpaceHeight()*inverseScale);
 	}
 
 
